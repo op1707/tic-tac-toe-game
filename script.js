8bb659882a5b3c1bf14376226e3f7dd3738ef11d
OM PRAKASH YADAV <105782993+op1707@users.noreply.github.com>
2022-09-01 17:49:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turn = "X"
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
 let isgameover = false;
 
-// Function to change the turn
 const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+    return turn === "X"? "0": "X";
 }
 
-// Function to check for a win
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
@@ -26,6 +22,7 @@ const checkWin = ()=>{
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            gameover.play();
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
@@ -33,9 +30,6 @@ const checkWin = ()=>{
         }
     })
 }
-
-// Game Logic
-// music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
@@ -52,7 +46,6 @@ Array.from(boxes).forEach(element =>{
     })
 })
 
-// Add onclick listener to reset button
 reset.addEventListener('click', ()=>{
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
